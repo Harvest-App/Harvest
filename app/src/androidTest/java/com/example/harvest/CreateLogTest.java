0fafe5077a141f29b372214218a88a30919e2565
Zagesh <1845347@students.wits.ac.za>
2022-09-28 14:15:01 +0200

    test

--- app/src/androidTest/java/com/example/harvest/CreateLogTest.java
+++ app/src/androidTest/java/com/example/harvest/CreateLogTest.java
@@ -1,44 +1,44 @@
 package com.example.harvest;
 
-//import static androidx.test.espresso.Espresso.onView;
-//import static androidx.test.espresso.assertion.ViewAssertions.matches;
-//import static androidx.test.espresso.matcher.ViewMatchers.withId;
-//import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-//
-//
-//import android.view.View;
-//
-//import androidx.test.core.app.ActivityScenario;
-//import androidx.test.ext.junit.rules.ActivityScenarioRule;
-//import androidx.test.ext.junit.runners.AndroidJUnit4;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
-//
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(AndroidJUnit4.class)
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+
+import android.view.View;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class CreateLogTest {
 
-//    @Rule
-//    public ActivityScenarioRule<CreateLog> createLogActivityScenarioRule = new ActivityScenarioRule<CreateLog>(CreateLog.class);
-//
-//    private ActivityScenario<CreateLog> createLog = null;
-//
-//    @Before
-//    public void setup() throws Exception{
-//        createLog = createLogActivityScenarioRule.getScenario();
-//    }
-//
-//    @Test
-//    public void isActivityInView(){
-//        onView(withId(R.id.createLog)).check(matches(isDisplayed()));
-//    }
-//
-//    @Test
-//    public void loginTextTest() {
-//        onView(withId(R.id.createInstruction)).check(matches(isDisplayed()));
-//    }
+    @Rule
+    public ActivityScenarioRule<CreateLog> createLogActivityScenarioRule = new ActivityScenarioRule<CreateLog>(CreateLog.class);
+
+    private ActivityScenario<CreateLog> createLog = null;
+
+    @Before
+    public void setup() throws Exception{
+        createLog = createLogActivityScenarioRule.getScenario();
+    }
+
+    @Test
+    public void isActivityInView(){
+        onView(withId(R.id.createLog)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void loginTextTest() {
+        onView(withId(R.id.createInstruction)).check(matches(isDisplayed()));
+    }
 }
