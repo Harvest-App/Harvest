1b5873f16af6fa025d54f641f61f91a068a31be6
Zagesh <1845347@students.wits.ac.za>
2022-10-08 16:10:53 +0200

    NullPointerException Change

---
+++ app/src/androidTest/java/com/example/harvest/ProfileActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.harvest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ProfileActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<ProfileActivity> profileActivityActivityScenarioRule = new ActivityScenarioRule<ProfileActivity>(ProfileActivity.class);
+
+    private ActivityScenario<ProfileActivity> profileActivity = null;
+
+    @Before
+    public void setup() throws Exception{
+
+        profileActivity = profileActivityActivityScenarioRule.getScenario();
+    }
+
+    @Test
+    public void isActivityInView(){
+        onView(withId(R.id.ProfileActivity)).check(matches(isDisplayed()));
+    }
+}
