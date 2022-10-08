a6af232ac57b6bd78ef0e4078077bb885acb848c
Zagesh <1845347@students.wits.ac.za>
2022-10-09 00:54:12 +0200

    NullPointerException Change

--- app/src/main/java/com/example/harvest/ProfileActivity.java
+++ app/src/main/java/com/example/harvest/ProfileActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -30,6 +31,8 @@ public class ProfileActivity extends AppCompatActivity {
     private Button logOut;
     private Button addLog;
     private TextView heading;
+
+    String currentUserID;
 
     //firestore database, documents and collection
     private FirebaseFirestore db;
@@ -95,7 +98,13 @@ public class ProfileActivity extends AppCompatActivity {
 
 
     private void EventChangeListener () {
-        db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).collection("Logs")
+        FirebaseUser mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(mFirebaseUser != null) {
+            currentUserID = mFirebaseUser.getUid(); //To avoid Null Pointer Exception
+            Toast.makeText(ProfileActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        db.collection("users").document(currentUserID).collection("Logs")
                 .orderBy("timeCreated", Query.Direction.DESCENDING)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
@@ -107,6 +116,7 @@ public class ProfileActivity extends AppCompatActivity {
                             Toast.makeText(ProfileActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
                             return;
                         }
+                        assert value != null;
                         for (DocumentChange dc : value.getDocumentChanges()) {
                             if (dc.getType() == DocumentChange.Type.ADDED) {
                                 OurLog thisLog=dc.getDocument().toObject(OurLog.class);
