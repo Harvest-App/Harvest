b279378b003b58bda41a199067b0c8f910c98895
Zagesh <1845347@students.wits.ac.za>
2022-10-09 16:28:10 +0200

    Profile Change

---
+++ app/src/androidTest/java/com/example/harvest/LogEntryHomeTest.java
@@ -0,0 +1,35 @@
+package com.example.harvest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LogEntryHomeTest {
+
+    @Rule
+    public ActivityScenarioRule<LogEntryHome> logEntryHomeActivityScenarioRule = new ActivityScenarioRule<LogEntryHome>(LogEntryHome.class);
+
+    private ActivityScenario<LogEntryHome> logEntryHome = null;
+
+    @Before
+    public void setup() throws Exception{
+
+        logEntryHome = logEntryHomeActivityScenarioRule.getScenario();
+    }
+
+    @Test
+    public void isActivityInView(){
+        onView(withId(R.id.pageLayout)).check(matches(isDisplayed()));
+    }
+}
