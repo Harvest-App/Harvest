890418dfe8288d11a377806d16b6d87fa23f688d
Zagesh <1845347@students.wits.ac.za>
2022-10-10 21:09:16 +0200

    Profile Change

--- app/src/androidTest/java/com/example/harvest/ForgotPasswordTest.java
+++ app/src/androidTest/java/com/example/harvest/ForgotPasswordTest.java
@@ -1,11 +1,14 @@
 package com.example.harvest;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -31,5 +34,24 @@ public class ForgotPasswordTest {
     @Test
     public void isActivityInView(){
         onView(withId(R.id.FOrgotPassword)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isEmailEmpty(){
+
+        onView(withId(R.id.email)).perform(ViewActions.typeText(""));
+        closeSoftKeyboard();
+        onView(withId(R.id.resetPassword)).perform(ViewActions.click());
+        onView(withId(R.id.email)).check(matches(hasErrorText("Email required")));
+
+    }
+
+    @Test
+    public void isEmailValid(){
+        onView(withId(R.id.email)).perform(ViewActions.typeText("123456com"));
+        closeSoftKeyboard();
+        onView(withId(R.id.resetPassword)).perform(ViewActions.click());
+        onView(withId(R.id.email)).check(matches(hasErrorText("Email invalid")));
+
     }
 }
