74eccc2aa3736f691c16874b4c4a002a72e75159
Zagesh <1845347@students.wits.ac.za>
2022-10-12 01:27:35 +0200

    new tests

--- app/src/androidTest/java/com/example/harvest/ProfileActivityTest.java
+++ app/src/androidTest/java/com/example/harvest/ProfileActivityTest.java
@@ -64,8 +64,7 @@ public class ProfileActivityTest {
     @Test
     public void isLogEntryClickSuccessful(){
         onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.click()));
-        Instrumentation.ActivityMonitor Lmonitor = getInstrumentation().addMonitor(LogEntryHome.class.getName(),null,false);
-        assertNotNull(Lmonitor);
+        onView(withId(R.id.pageLayout)).check(matches(isDisplayed()));
 
     }
 
