87cd28cfc7c963d75f7acc52ebfda671524d1846
Zagesh <1845347@students.wits.ac.za>
2022-10-11 19:22:41 +0200

    new tests

--- app/src/androidTest/java/com/example/harvest/ProfileActivityTest.java
+++ app/src/androidTest/java/com/example/harvest/ProfileActivityTest.java
@@ -58,13 +58,13 @@ public class ProfileActivityTest {
 
     }
 
-//    @Test
-//    public void isSelectLogSuccessful(){
-//        onView(withId(R.layout.class_profile_log)).perform(ViewActions.click());
-//        Instrumentation.ActivityMonitor LandingMonitor = getInstrumentation().addMonitor(LogEntryHome.class.getName(),null,false);
-//        assertNotNull(LandingMonitor);
-//
-//    }
+    @Test
+    public void isSelectLogSuccessful(){
+        onView(withId(R.layout.class_profile_log)).perform(ViewActions.click());
+        Instrumentation.ActivityMonitor LandingMonitor = getInstrumentation().addMonitor(LogEntryHome.class.getName(),null,false);
+        assertNotNull(LandingMonitor);
+
+    }
 
     @After
     public void tearDown() throws Exception {
